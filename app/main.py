bc272ba6ca17da49e341e41fd10d23459c5a5b96
akashbharatshejul <shejulakash777@gmail.com>
2026-03-05 14:47:37 +0530

    Day 2: Add database connection and User model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.database.database import engine, Base
+from app.models import user_model
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
